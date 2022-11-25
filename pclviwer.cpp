76940a03e9f456bae727b80caea3578e855cf52a
lnwin <405824853@qq.com>
2022-11-25 17:39:45 +0800

    完成3D点云的窗口嵌入和鼠标互动

--- pclviwer.cpp
+++ pclviwer.cpp
@@ -1,68 +1,69 @@
 ﻿#include "pclviwer.h"
 
+vtkSmartPointer<vtkActor> addCloudPoints(pcl::PointCloud<pcl::PointXYZRGBA> &cloud)
+{
+    vtkSmartPointer<vtkPoints>points = vtkSmartPointer<vtkPoints>::New();//点数据
+    vtkSmartPointer<vtkCellArray> vertices = vtkSmartPointer<vtkCellArray>::New();//单元数据 顶点类型
+
+    for (int i = 0; i < cloud.points.size();i++)
+    {
+        //定义存储顶点索引的中间变量，类似Int、long类型
+        vtkIdType pointId[1];
+        //将每个点的坐标加入vtkPoints，InsertNextPoint()返回加入点的索引号
+        pointId[0] = points->InsertNextPoint(cloud.points[i].x, cloud.points[i].y, cloud.points[i].z);
+        //为每个坐标点分别创建一个顶点，顶点是单元类型里面的一种
+        vertices->InsertNextCell(1,pointId);
+    }
+    //创建vtkPolyData对象
+    vtkSmartPointer<vtkPolyData> polyData = vtkSmartPointer<vtkPolyData>::New();
+    //指定数据的几何结构（由points指定）和拓扑结构（由vertices指定）
+    polyData->SetPoints(points);
+    polyData->SetVerts(vertices);
+
+    vtkSmartPointer<vtkPolyDataMapper> pointsMapper =vtkSmartPointer<vtkPolyDataMapper>::New();
+    pointsMapper->SetInputData(polyData);
+
+    vtkSmartPointer<vtkActor> pointsActor = vtkSmartPointer<vtkActor>::New();
+    pointsActor->SetMapper(pointsMapper);
+    pointsActor->GetProperty()->SetPointSize(3);//设置点的大小
+
+    return pointsActor;
+}
+
 PCLViwer::PCLViwer(Ui::MainWindow ui)
 {
-//        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>); //创建存放读取点云的对象
-//        if (pcl::io::loadPCDFile("C:/Users/Administrator/Desktop/bunny.pcd", *cloud_in) < 0)
-//        {
-//            PCL_ERROR("\a->点云文件不存在！\n");
-//           // system("pause");
-//        }
-//        cout << "->加载了 " << cloud_in->points.size() << " 个数据点" << endl;
-//        boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
-//        viewer->addPointCloud(cloud_in, "cloud");
-
-//        vtkNew<vtkGenericOpenGLRenderWindow> myrenderWindow;
-
-//        ui.openGLWidget->SetRenderWindow(myrenderWindow);
-////            viewer->setupInteractor(ui.qvtkWidget->GetInteractor(), ui.qvtkWidget->GetRenderWindow());
-////            viewer->setCameraPosition(0, 0, -2, 0, -1, 0, 0);
-//        ui.openGLWidget->update();
-
+        pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZRGBA>); //创建存放读取点云的对象
+        if (pcl::io::loadPCDFile("C:/Users/Administrator/Desktop/bunny.pcd", *cloud_in) < 0)
+        {
+            PCL_ERROR("\a->点云文件不存在！\n");
+           // system("pause");
+        }
+        cout << "->加载了 " << cloud_in->points.size() << " 个数据点" << endl;
+//        pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
+//            string pathData = "../data/underWater.pcd";
         QVTKOpenGLWidget *qvtk=ui.openGLWidget;
-
         // Setup window
         vtkSmartPointer<vtkGenericOpenGLRenderWindow> renderWindow =
-            vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
-        // setup sphere
-        vtkSmartPointer<vtkSphereSource> sphereSource =
-                vtkSmartPointer<vtkSphereSource>::New();
-            sphereSource->SetCenter(0.0, 0.0, 0.0);
-            sphereSource->SetRadius(5.0);
-            sphereSource->Update();
-        // setup map and actor
-        vtkSmartPointer<vtkPolyDataMapper> mapper =
-            vtkSmartPointer<vtkPolyDataMapper>::New();
-        mapper->SetInputConnection(sphereSource->GetOutputPort());
-
-        vtkSmartPointer<vtkActor> actor =
-            vtkSmartPointer<vtkActor>::New();
-        actor->SetMapper(mapper);
-
-        vtkSmartPointer<vtkNamedColors> colors =
-            vtkSmartPointer<vtkNamedColors>::New();
-
+            vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();       // setup sphere
+        qvtk->SetRenderWindow(renderWindow);
+        vtkNew<vtkRenderWindowInteractor> interactor;
+          interactor->SetRenderWindow(renderWindow);
+        vtkSmartPointer<vtkActor> pointsActor = addCloudPoints(*cloud_in);
         // Setup renderer
         vtkSmartPointer<vtkRenderer> renderer =
-        vtkSmartPointer<vtkRenderer>::New();
-        renderer->SetBackground(colors->GetColor3d("Slate_grey").GetData());
+            vtkSmartPointer<vtkRenderer>::New();
+        renderer->SetBackground(.1, .2, .3);
+        renderer->AddActor(pointsActor);
         renderWindow->AddRenderer(renderer);
-        renderer->AddActor(actor);
-        qvtk->SetRenderWindow(renderWindow);
-        // create stuff for user interaction
-        vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
-        vtkSmartPointer<vtkRenderWindowInteractor>::New();
-        /*
-          vtkSmartPointer<vtkUserInteractionStyle> inter = vtkUserInteractionStyle::New ();
-           inter->SetDefaultRenderer(renderer);
-           renderWindowInteractor->SetInteractorStyle( inter );
-        */
+        vtkSmartPointer<vtkInteractorStyleTrackballCamera>interactorStyle = vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New();
+            interactor->SetInteractorStyle(interactorStyle);
+      //  qvtk->show();
+       // renderWindow->SetSize(70,80);
+        renderWindow->Render();
+        interactor->Initialize();
 
-        // make the link between the  renderwindow and the interactor
-        renderWindowInteractor->SetRenderWindow ( renderWindow );
-        qvtk->show();
-        renderWindow->Render ();
-        renderWindowInteractor->Initialize();
+
+;
 
 
 
--- pclviwer.h
+++ pclviwer.h
@@ -4,12 +4,25 @@
 #include <pcl/io/pcd_io.h> //PCD读写类相关头文件
 #include <pcl/point_types.h> //PCL中支持的点类型头文件
 #include <pcl/visualization/cloud_viewer.h>
-#include <vtkRenderWindow.h>
 #include <vtkGenericOpenGLRenderWindow.h>
 #include <vtkNamedColors.h>
-#include <vtkPolyDataMapper.h>
+#include <vtkPoints.h>
 using namespace pcl;
 #include "vtkAutoInit.h"
+#include"vtkSmartPointer.h"
+#include"vtkAutoInit.h"
+#include"vtkRenderer.h"
+#include"vtkRenderWindow.h"
+#include"vtkRenderWindowInteractor.h"
+#include"vtkPolyData.h"
+#include"vtkPolyDataReader.h"
+#include"vtkSurfaceReconstructionFilter.h"
+#include"vtkActor.h"
+#include"vtkCamera.h"
+#include"vtkContourFilter.h"
+#include"vtkPolyDataMapper.h"
+#include"vtkVertexGlyphFilter.h"
+#include"vtkProperty.h"
 
 VTK_MODULE_INIT(vtkRenderingOpenGL2); // VTK was built with vtkRenderingOpenGL
 VTK_MODULE_INIT(vtkInteractionStyle);
@@ -19,6 +32,7 @@ class PCLViwer
 
 public:
     PCLViwer(Ui::MainWindow);
+   // void vtkSmartPointer<vtkActor> addCloudPoints(pcl::PointCloud<pcl::PointXYZRGBA> &cloud);
     //typedef pcl::PointXYZRGB PointT; //定义点云的每个点的类型；pcl::PointXYZRGBA ：包括空间坐标XYZ以及颜色信息RGB，多了一个alpha通道；PointXYZHSV :空间位置 + HSV颜色空间表示的颜色
    // typedef pcl::PointCloud<PointT> PointClouds; //定义点云类，用于创建点云对象
 
